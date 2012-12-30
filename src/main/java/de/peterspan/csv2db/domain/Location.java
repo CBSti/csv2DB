5cba4a82c9663510e6064f7b456a5a1930b41b24
Frederik Hahne <frederik.hahne@googlemail.com>
2012-12-30 21:35:20 +0100

    First simple Version of pojos.

---
+++ src/main/java/de/peterspan/csv2db/domain/Location.java
@@ -0,0 +1,15 @@
+package de.peterspan.csv2db.domain;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Location implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 6536256817404725224L;
+
+}
