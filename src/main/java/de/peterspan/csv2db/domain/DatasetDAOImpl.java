d086630a825f4293d0b9a58d5b30f15fe0965e2f
Frederik Hahne <frederik.hahne@googlemail.com>
2013-01-01 19:54:00 +0100

    Dao Impl that can implement special methods in addition to the ones generated by HEDL.

---
+++ src/main/java/de/peterspan/csv2db/domain/DatasetDAOImpl.java
@@ -0,0 +1,10 @@
+package de.peterspan.csv2db.domain;
+
+import org.springframework.stereotype.Component;
+
+import de.peterspan.csv2db.domain.dao.DataSetDAO;
+
+@Component
+public class DatasetDAOImpl extends DataSetDAO {
+
+}
---
+++ src/main/java/de/peterspan/csv2db/domain/MeasurementValuesDAOImpl.java
@@ -0,0 +1,10 @@
+package de.peterspan.csv2db.domain;
+
+import org.springframework.stereotype.Component;
+
+import de.peterspan.csv2db.domain.dao.MeasurementValuesDAO;
+
+@Component
+public class MeasurementValuesDAOImpl extends MeasurementValuesDAO {
+
+}
