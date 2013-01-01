ff5ed8dab3bc03efc0e057106ccf8c65520c8e4d
Frederik Hahne <frederik.hahne@googlemail.com>
2013-01-01 16:26:31 +0100

    Added License Header.

--- src/main/java/de/peterspan/csv2db/AppWindow.java
+++ src/main/java/de/peterspan/csv2db/AppWindow.java
@@ -1,3 +1,21 @@
+/**
+ *  Copyright 2012-2013 Frederik Hahne, Christoph Stiehm
+ *
+ * 	This file is part of csv2db.
+ *
+ *  csv2db is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  csv2db is distributed in the hope that it will be useful,
+ * 	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with csv2db.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package de.peterspan.csv2db;
 
 import java.awt.BorderLayout;
