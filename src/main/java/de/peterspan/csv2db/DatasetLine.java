eea6f55b58737685d0ab9393f20886b7199b4e37
Frederik Hahne <frederik.hahne@googlemail.com>
2013-01-01 20:03:56 +0100

    Wrapper Class to wrap a simple string array into a more "integelligent" class to retrieve data values for later database.

---
+++ src/main/java/de/peterspan/csv2db/DatasetLine.java
@@ -0,0 +1,35 @@
+/**
+ *  Copyright 2012-2013 Frederik Hahne, Christoph Stiehm
+ *
+ * 	This file is part of csv2db.
+ *
+ *  csv2db is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  csv2db is distributed in the hope that it will be useful,
+ * 	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with csv2db.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package de.peterspan.csv2db;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DatasetLine {
+
+	List<String> dataValues;
+	
+	public DatasetLine(String[] values){
+		dataValues = new ArrayList<String>();
+		
+		for(int i=0;i<values.length;i++) {
+			dataValues.add(values[i]);
+		}
+	}
+}
