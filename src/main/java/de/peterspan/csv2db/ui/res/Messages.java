4b2831ce53f41eea1482cba31b59b2cc6511dbbe
Frederik Hahne <frederik.hahne@googlemail.com>
2013-01-01 13:35:46 +0100

    Corrected Resource Bundle integration.

--- src/main/java/de/peterspan/csv2db/ui/res/Messages.java
+++ src/main/java/de/peterspan/csv2db/ui/res/Messages.java
@@ -23,7 +23,7 @@ import java.util.MissingResourceException;
 import java.util.ResourceBundle;
 
 public class Messages {
-	private static final String BUNDLE_NAME = "de.atomfrede.tools.evalutation.ui.res.messages"; //$NON-NLS-1$
+	private static final String BUNDLE_NAME = "de.peterspan.csv2db.ui.res.messages"; //$NON-NLS-1$
 
 	private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle.getBundle(BUNDLE_NAME);
 
